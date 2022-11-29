4bfe7f5000dd9af7b7a7673764a11743fc949f4e
George Fernandez <fernandezg704@gmail.com>
2022-11-29 12:05:43 -0500

    installed packages for icons

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import HeaderNav from "./HeaderNav";
+
+function App() {
+  return (
+    <div className="App">
+      <HeaderNav />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HeaderNav.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function HeaderNav() {
+  return (
+    <div className='headerNav'>
+        <div id='iconLeft'>
+            <p>Icon</p>
+        </div>
+        <div id='logoMiddle'>
+            <img src='https://via.placeholder.com/280' alt='logo' />
+        </div>
+        <div id='iconRight'>
+            <p>Icon</p>
+        </div>
+    </div>
+  )
+}
+
+export default HeaderNav
