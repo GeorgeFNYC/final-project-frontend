27cce238646da4abc78d8948a6a6a01dd04880d7
George Fernandez <fernandezg704@gmail.com>
2022-11-29 14:39:42 -0500

    Adjusting elements in the header

--- src/components/HeaderNav.js
+++ src/components/HeaderNav.js
@@ -1,16 +1,22 @@
 import React from 'react'
+import { BiMap } from 'react-icons/bi'
+import { CgProfile } from 'react-icons/cg'
 
 function HeaderNav() {
   return (
     <div className='headerNav'>
         <div id='iconLeft'>
-            <p>Icon</p>
+            <a href='/map'>
+                <BiMap className='headerIcons'/>
+            </a>
         </div>
         <div id='logoMiddle'>
             <img src='https://via.placeholder.com/280' alt='logo'/>
         </div>
         <div id='iconRight'>
-            <p>Icon</p>
+            <a href='/profile'>
+                <CgProfile className='headerIcons'/>
+            </a>
         </div>
     </div>
   )
