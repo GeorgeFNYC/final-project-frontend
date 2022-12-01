23ac4e651eb9215719ba3fdae452a3c6eafc033e
George Fernandez <fernandezg704@gmail.com>
2022-12-01 11:06:46 -0500

    Links added for the home page nad header

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,27 @@ import HeaderNav from './HeaderNav'
 
 function Home({ isSignedIn }) {
   return (
+    <>
     <HeaderNav isSignedIn={isSignedIn} />
+
+    <div className='websiteQuestion'>
+      <h1>Website question</h1>
+    </div>
+    <div id='partContainer'>
+      <div id='armDiv'>
+        <a href='/'>Arm</a>
+      </div>
+      <div id='chestDiv'>
+        <a href='/'>Chest</a>
+      </div>
+      <div id='stomachDiv'>
+        <a href='/'>Stomach</a>
+      </div>
+      <div id='legDiv'>
+        <a href='/'>Leg</a>
+      </div>
+    </div>
+  </>
   )
 }
 
