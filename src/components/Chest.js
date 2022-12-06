75b491e47718e2aece5dc0b1a53a2eb6f07e0d72
George Fernandez <fernandezg704@gmail.com>
2022-12-06 15:29:53 -0500

    data for chest, fetched

--- src/components/Chest.js
+++ src/components/Chest.js
@@ -2,12 +2,31 @@ import React from 'react'
 
 import HeaderNav from './HeaderNav'
 
-function Chest() {
+function Chest({ workouts }) {
+
+  const myStyle = {
+    width: '20%',
+    height: 'auto',
+  }
+
+
   return (
     <>
         <HeaderNav />
         <h1>Chest</h1>
-        
+        <div className='workoutsDiv'>
+          <div className='workouts'>
+            {workouts.slice(6, 9).map(chest => {
+              return (
+                <>
+                  <h3>{chest.name}</h3>
+                  <video style={myStyle} src={chest.video_clip}></video>
+                  <p>{chest.workout_description}</p>
+                </>
+              )
+            })}
+          </div>
+        </div>
     </>
   )
 }
